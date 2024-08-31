137a79e006c3c30244b86091820a4de484437f88
devjoseotavio <jose.gallo@Isolutions.com.br>
2024-08-31 19:09:03 -0300

    Ajustando estrutura pra criação de um projeto de criação de tarefas | notas em react - pt1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [message, setMessage] = useState("Hello World");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div>
+      <div className="container">{message}</div>
+      <button onClick={() => setMessage("Hello")}>
+          Mudar msg
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+export default App 

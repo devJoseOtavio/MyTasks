278f6d41b9e98e1891896fc49cc64e713d31dc94
devjoseotavio <jose.gallo@Isolutions.com.br>
2024-09-10 21:11:53 -0300

    criando seleção de tarefa e deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 import "./App.css";
 import Tasks from "./components/Tasks";
@@ -17,12 +18,38 @@ const App = () => {
       completed: true,
     }
   ]);
+
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map(task => {
+      if (task.id == taskId) return { ... task, completed: !task.completed } 
+
+      return task;
+    });
+
+    setTasks(newTasks)
+  }
+
+  const handleTaskAdditional = (taskTitle) => {
+    const newTask = [ ...tasks, {
+      title: taskTitle,
+      id: uuidv4(),
+      completed: false
+    }]
+    
+    setTasks(newTask)
+  }
+
+  const handleTaskDeletion = (taskId) => {
+    const newTasks = tasks.filter( task => task.id != taskId)
+    setTasks(newTasks)
+  }
+
   return (
   
     <div>
       <div className="container">
-        <AddTask />
-        <Tasks tasks={tasks}/>
+        <AddTask handleTaskAdditional={handleTaskAdditional} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDeletion={handleTaskDeletion}/>
       </div>
     </div>
   );
